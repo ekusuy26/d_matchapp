f9736a6d8406dbeaf9e22435ae837046b4c72ca9
ekusuy26 <y.yuu6221@gmail.com>
2020-07-04 22:59:46 +0900

    Completed implementation of user functions

---
+++ accounts/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+ 
+app_name='accounts'
+ 
+urlpatterns = [
+    path('accounts/login/', views.MyLoginView.as_view(), name="login"),
+    path('accounts/logout/', views.MyLogoutView.as_view(), name="logout"),
+    path('accounts/create/', views.UserCreateView.as_view(),name="create"),
+]
+
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import TemplateView,CreateView,DeleteView, UpdateView
+from django.urls import reverse_lazy, reverse
+from . import forms
 
 # Create your views here.
+
+class MyLoginView(LoginView):
+    form_class = forms.LoginForm
+    template_name = "accounts/login.html"
+
+class MyLogoutView(LoginRequiredMixin, LogoutView):
+    template_name = "myhp/topPage.html"
+
+class UserCreateView(CreateView):
+    form_class = UserCreationForm
+    template_name = "accounts/create.html"
+    success_url = reverse_lazy("accounts:login")
