e623d58c38a31d25c9156c581ecb282f2d18047e
ekusuy26 <y.yuu6221@gmail.com>
2020-07-05 02:24:26 +0900

    Change the display of the sidebar depending on whether a pet is registered

--- myhp/views.py
+++ myhp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from accounts.models import Dog
 
 # Create your views here.
 
@@ -6,4 +7,11 @@ def topPage(request):
     return render(request, 'myhp/topPage.html', {})
 
 def index(request):
-    return render(request, 'myhp/index.html', {})
+    dog = Dog.objects.filter(user_id=request.user.id)
+    if dog.count() == 0:
+        dog_flg = 0
+    else:
+        dog_flg = 1
+    return render(request, 'myhp/index.html', {
+        'dog_flg': dog_flg,
+    })
