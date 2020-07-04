c48ba35d9d30a3f6a2b1f1166a51d0d0f99a6140
ekusuy26 <y.yuu6221@gmail.com>
2020-07-05 02:14:39 +0900

    create dog table

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Dog(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    dogname = models.CharField(max_length=100)
+    age = models.IntegerField()
+    sex = models.CharField(max_length=100)
+    introduction = models.CharField(max_length=255)
+    image = models.ImageField(upload_to="dogs/")
+    like_num = models.IntegerField(default=0)
