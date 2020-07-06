9259c7f786f5914ee5341f2f5c3396dd0113ba52
ekusuy26 <y.yuu6221@gmail.com>
2020-07-06 16:29:52 +0900

    Change app name

--- chats/views.py
+++ chats/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'messages/index.html', {})
+    return render(request, 'chats/index.html', {})
--- config/urls.py
+++ config/urls.py
@@ -22,7 +22,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
-    path('', include('messages.urls')),
+    path('', include('chats.urls')),
     path('', include('myhp.urls')),
 ]
 
