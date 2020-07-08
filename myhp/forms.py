8e7c65b6f8f3380fb1bc80dddb78755ea63a1f44
ekusuy26 <y.yuu6221@gmail.com>
2020-07-08 17:03:36 +0900

    Describe fusion process in view

---
+++ myhp/forms.py
@@ -0,0 +1,3 @@
+from django.contrib.auth import forms as auth_forms
+from django import forms
+from .models import Image
--- myhp/views.py
+++ myhp/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, redirect
 from accounts.models import Dog
 from django.core.paginator import Paginator
+from . import forms
+from .models import Image
+from PIL import Image
+import cv2
+from django.conf import settings
 
 # Create your views here.
 
@@ -36,8 +41,46 @@ def fusion(request, pk):
     })
 
 def result(request, pk):
-    opponent = Dog.objects.get(id = pk)
     headLine = '結果'
+    myself = Dog.objects.get(user_id = request.user.id)
+    opponent = Dog.objects.get(id = pk)
+    input_path = settings.BASE_DIR + myself.image.url
+    input_path_two = settings.BASE_DIR + opponent.image.url
+    output_path = settings.BASE_DIR + "/media/results/output" + str(request.user.id) + str(pk) + ".jpg"
+    output_path_two = settings.BASE_DIR + "/media/results/output_two" + str(request.user.id) + str(pk) + ".jpg"
+    src = cv2.imread(input_path)
+    src_two = cv2.imread(input_path_two)
+    img = src.copy()
+    img_two = src_two.copy()
+    def face_swap(src, src_two, x, y, width, height, ratio=1):
+        dst = src.copy()
+        dst_02 = src_two.copy()
+        dst[y:y + height, x:x + width] = dst_02[y:y + height, x:x + width]
+        return dst
+    face_cascade_path = './haarcascade_frontalcatface.xml'
+    face_cascade = cv2.CascadeClassifier(face_cascade_path)
+    face_cascade_two = cv2.CascadeClassifier(face_cascade_path)
+    src_gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    src_two_gray = cv2.cvtColor(src_two, cv2.COLOR_BGR2GRAY)
+    img_two_gray = cv2.cvtColor(img_two, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(src_gray)
+    faces_two = face_cascade_two.detectMultiScale(img_two_gray)
+    for x, y, w, h in faces:
+        for a, b, c, d in faces_two:
+            dst_face_01 = face_swap(src, src_two, x, y, w, h)
+            dst_face_02 = face_swap(img_two, img, a, b, c, d)
+    
+            cv2.imwrite(output_path, dst_face_01)
+            cv2.imwrite(output_path_two, dst_face_02)
+    img_tbl = Image()
+    img_tbl.first_image = output_path
+    img_tbl.second_image = output_path_two
+    img_tbl.save()
+    new_image = Image.objects.latest('id')
+    new_image.users.add(request.user)
+    new_image.users.add(opponent.user.id)
+
     return render(request, 'myhp/result.html', {
         'opponent': opponent,
         'headLine': headLine,
