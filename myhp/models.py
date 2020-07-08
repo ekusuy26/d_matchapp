db1b152f0efd501200b00ff40c87f69c47874e6f
ekusuy26 <y.yuu6221@gmail.com>
2020-07-08 16:06:49 +0900

    create image table

--- myhp/models.py
+++ myhp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Image(models.Model):
+    users = models.ManyToManyField(User, verbose_name='ユーザー')
+    first_image = models.ImageField(upload_to='results/', default='defo')
+    second_image = models.ImageField(upload_to='results/', default='defo')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
